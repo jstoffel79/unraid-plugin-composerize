6a21f08add187f2dedc8e0a9017f1510deb8081b
James Stoffel <120999934+jstoffel79@users.noreply.github.com>
2025-08-14 21:53:59 -0400

    Update test.sh

--- plugin/test.sh
+++ plugin/test.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# This is a simple test script to verify that the Unraid web server can execute
+# scripts from within the plugin's directory.
 
+# Write a confirmation message to the main Unraid system log.
+logger "Composerize API Test: Shell script executed successfully."
